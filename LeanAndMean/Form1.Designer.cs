45579839465be2b0e7f4258baaf2ffc77087dc36
Joseph Cai <joseph_cai@my.uri.edu>
2019-01-31 21:35:32 -0600

    initial commit

---
+++ LeanAndMean/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace LeanAndMean
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.femaleButton = new System.Windows.Forms.RadioButton();
+            this.maleButton = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.bmiOutputLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.weightTextbox = new System.Windows.Forms.TextBox();
+            this.heightftTextbox = new System.Windows.Forms.TextBox();
+            this.heightinTextbox = new System.Windows.Forms.TextBox();
+            this.ageTextbox = new System.Windows.Forms.TextBox();
+            this.lossPerWeekComboBox = new System.Windows.Forms.ComboBox();
+            this.activityLevelComboBox = new System.Windows.Forms.ComboBox();
+            this.caloriesOutputLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight (lbs): ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ft.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "in.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Age:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.femaleButton);
+            this.groupBox1.Controls.Add(this.maleButton);
+            this.groupBox1.Location = new System.Drawing.Point(10, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(142, 48);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sex";
+            // 
+            // femaleButton
+            // 
+            this.femaleButton.AutoSize = true;
+            this.femaleButton.Location = new System.Drawing.Point(71, 19);
+            this.femaleButton.Name = "femaleButton";
+            this.femaleButton.Size = new System.Drawing.Size(59, 17);
+            this.femaleButton.TabIndex = 1;
+            this.femaleButton.TabStop = true;
+            this.femaleButton.Text = "Female";
+            this.femaleButton.UseVisualStyleBackColor = true;
+            // 
+            // maleButton
+            // 
+            this.maleButton.AutoSize = true;
+            this.maleButton.Location = new System.Drawing.Point(14, 19);
+            this.maleButton.Name = "maleButton";
+            this.maleButton.Size = new System.Drawing.Size(48, 17);
+            this.maleButton.TabIndex = 0;
+            this.maleButton.TabStop = true;
+            this.maleButton.Text = "Male";
+            this.maleButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Desired Weight Loss / Week:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(87, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Activity Level:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(132, 341);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "BMI Score:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 389);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(194, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Calories you should consume each day:";
+            // 
+            // bmiOutputLabel
+            // 
+            this.bmiOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bmiOutputLabel.Location = new System.Drawing.Point(209, 335);
+            this.bmiOutputLabel.Name = "bmiOutputLabel";
+            this.bmiOutputLabel.Size = new System.Drawing.Size(103, 25);
+            this.bmiOutputLabel.TabIndex = 10;
+            this.bmiOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(41, 459);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 12;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(135, 459);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 13;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(226, 459);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 14;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // weightTextbox
+            // 
+            this.weightTextbox.Location = new System.Drawing.Point(81, 6);
+            this.weightTextbox.Name = "weightTextbox";
+            this.weightTextbox.Size = new System.Drawing.Size(100, 20);
+            this.weightTextbox.TabIndex = 15;
+            // 
+            // heightftTextbox
+            // 
+            this.heightftTextbox.Location = new System.Drawing.Point(81, 48);
+            this.heightftTextbox.Name = "heightftTextbox";
+            this.heightftTextbox.Size = new System.Drawing.Size(100, 20);
+            this.heightftTextbox.TabIndex = 16;
+            // 
+            // heightinTextbox
+            // 
+            this.heightinTextbox.Location = new System.Drawing.Point(209, 52);
+            this.heightinTextbox.Name = "heightinTextbox";
+            this.heightinTextbox.Size = new System.Drawing.Size(100, 20);
+            this.heightinTextbox.TabIndex = 17;
+            // 
+            // ageTextbox
+            // 
+            this.ageTextbox.Location = new System.Drawing.Point(81, 85);
+            this.ageTextbox.Name = "ageTextbox";
+            this.ageTextbox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextbox.TabIndex = 18;
+            // 
+            // lossPerWeekComboBox
+            // 
+            this.lossPerWeekComboBox.FormattingEnabled = true;
+            this.lossPerWeekComboBox.Items.AddRange(new object[] {
+            "Half a pound per week.",
+            "One pound per week.",
+            "Two pounds per week."});
+            this.lossPerWeekComboBox.Location = new System.Drawing.Point(166, 197);
+            this.lossPerWeekComboBox.Name = "lossPerWeekComboBox";
+            this.lossPerWeekComboBox.Size = new System.Drawing.Size(121, 21);
+            this.lossPerWeekComboBox.TabIndex = 19;
+            // 
+            // activityLevelComboBox
+            // 
+            this.activityLevelComboBox.FormattingEnabled = true;
+            this.activityLevelComboBox.Items.AddRange(new object[] {
+            "Sedentary (Office Job)",
+            "Light (1 - 2 days per week)",
+            "Moderate (3 - 5 days per week",
+            "Heavy (6 - 7 days per week",
+            "Athlete (Twice a day)"});
+            this.activityLevelComboBox.Location = new System.Drawing.Point(166, 240);
+            this.activityLevelComboBox.Name = "activityLevelComboBox";
+            this.activityLevelComboBox.Size = new System.Drawing.Size(121, 21);
+            this.activityLevelComboBox.TabIndex = 20;
+            // 
+            // caloriesOutputLabel
+            // 
+            this.caloriesOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.caloriesOutputLabel.Location = new System.Drawing.Point(209, 383);
+            this.caloriesOutputLabel.Name = "caloriesOutputLabel";
+            this.caloriesOutputLabel.Size = new System.Drawing.Size(103, 25);
+            this.caloriesOutputLabel.TabIndex = 21;
+            this.caloriesOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 503);
+            this.Controls.Add(this.caloriesOutputLabel);
+            this.Controls.Add(this.activityLevelComboBox);
+            this.Controls.Add(this.lossPerWeekComboBox);
+            this.Controls.Add(this.ageTextbox);
+            this.Controls.Add(this.heightinTextbox);
+            this.Controls.Add(this.heightftTextbox);
+            this.Controls.Add(this.weightTextbox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.bmiOutputLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label bmiOutputLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox weightTextbox;
+        private System.Windows.Forms.TextBox heightftTextbox;
+        private System.Windows.Forms.TextBox heightinTextbox;
+        private System.Windows.Forms.TextBox ageTextbox;
+        private System.Windows.Forms.RadioButton femaleButton;
+        private System.Windows.Forms.RadioButton maleButton;
+        private System.Windows.Forms.ComboBox lossPerWeekComboBox;
+        private System.Windows.Forms.ComboBox activityLevelComboBox;
+        private System.Windows.Forms.Label caloriesOutputLabel;
+    }
+}
+
---
+++ LeanAndMean/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeanAndMean
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            //Set Variables
+            int weightPounds = 0, heightFeet = 0, heightInches = 0, age = 0, amountToLoseMultiplier = 0, maleMultiplier = 66, femaleMultipler = 655;
+            bool male = false;
+            bool female = false;
+            string lossPerWeek = "";
+            string activityLevel = "";
+            float activityMultiplier = 0, weightConv, heightConv, maleBMR, femaleBMR, maleTDEE, femaleTDEE, BMI;
+            const float IN_TO_CM = 2.54f;
+            const float LB_TO_KG = 0.453592f;
+
+
+            //Exception Handling
+            try
+            {
+                weightPounds = int.Parse(weightTextbox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid weight.");
+            }
+           
+            try
+            {
+                heightFeet = int.Parse(heightftTextbox.Text);
+                heightInches = int.Parse(heightinTextbox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid height.");
+            }
+
+            try
+            {
+                age = int.Parse(ageTextbox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter a valid age.");
+            }
+            try
+            {
+                if (maleButton.Checked)
+                {
+                    male = true;
+                }
+                else if (femaleButton.Checked)
+                {
+                    female = true;
+                }
+                else
+                {
+                    throw new GenderNotFoundException("Please select a gender");
+                }
+            }
+            catch (GenderNotFoundException msg)
+            {
+                MessageBox.Show(msg.Message);
+            }
+
+            try
+            {
+                if (lossPerWeekComboBox.SelectedIndex != -1)
+                {
+                    lossPerWeek = lossPerWeekComboBox.SelectedItem.ToString();
+                }
+                else
+                {
+                    throw new LossAmountNotFoundException("Please select an amount to lose.");
+                }
+            }
+            catch (LossAmountNotFoundException msg)
+            {
+                MessageBox.Show(msg.Message);
+            }
+
+            try
+            {
+                if (activityLevelComboBox.SelectedIndex != -1)
+                {
+                    activityLevel = activityLevelComboBox.SelectedItem.ToString();
+
+                }
+                else
+                {
+                    throw new ActivityLevelNotFoundException("Please select an activity level.");
+                }
+            }
+            catch (ActivityLevelNotFoundException msg)
+            {
+                MessageBox.Show(msg.Message);
+            }
+
+            switch (lossPerWeek)
+            {
+                case "Half a pound per week.":
+                    amountToLoseMultiplier = 250;
+                    break;
+                case "One pound per week.":
+                    amountToLoseMultiplier = 500;
+                    break;
+                case "Two pounds per week.":
+                    amountToLoseMultiplier = 1000;
+                    break;
+                default:
+                    amountToLoseMultiplier = 0;
+                    break;
+            }
+
+            switch (activityLevel)
+            {
+                case "Sedentary (Office Job)":
+                    activityMultiplier = 1.2f;
+                    break;
+                case "Light (1 - 2 days per week)":
+                    activityMultiplier = 1.375f;
+                    break;
+                case "Moderate (3 - 5 days per week)":
+                    activityMultiplier = 1.55f;
+                    break;
+                case "Heavy (6 - 7 days per week)":
+                    activityMultiplier = 1.725f;
+                    break;
+                case "Athlete (Twice a day)":
+                    activityMultiplier = 1.9f;
+                    break;
+
+            }
+
+            // Conversions
+            heightConv = ((heightFeet * 12) + heightInches) * IN_TO_CM;
+            weightConv = weightPounds * LB_TO_KG;
+
+            //Calculate BMI
+            BMI = (weightPounds * 703) / (((heightFeet * 12) + heightInches) * ((heightFeet * 12) + heightInches));
+            bmiOutputLabel.Text = BMI.ToString();
+
+            //Calculates TDEE
+            if (male == true)
+            {
+                maleBMR = maleMultiplier + (13.7f * weightConv) + (5 * heightConv) - (6.8f * age);
+                maleTDEE = (maleBMR * activityMultiplier) - amountToLoseMultiplier;
+
+                //Print details
+                caloriesOutputLabel.Text = maleTDEE.ToString();
+
+            }
+            else if (female == true)
+            {
+                femaleBMR = femaleMultipler + (9.6f * weightConv) + (1.8f * heightConv) - (4.7f * age);
+                femaleTDEE = (femaleBMR * activityMultiplier) - amountToLoseMultiplier;
+
+                //Print details
+                caloriesOutputLabel.Text = femaleTDEE.ToString();
+            }
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            weightTextbox.Text = " ";
+            heightftTextbox.Text = " ";
+            heightinTextbox.Text = " ";
+            ageTextbox.Text = " ";
+            bmiOutputLabel.Text = " ";
+            caloriesOutputLabel.Text = " ";
+            activityLevelComboBox.SelectedIndex = -1;
+            lossPerWeekComboBox.SelectedIndex = -1;
+
+        }
+    }
+
+    [Serializable]
+    internal class ActivityLevelNotFoundException : Exception
+    {
+        public ActivityLevelNotFoundException()
+        {
+        }
+
+        public ActivityLevelNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ActivityLevelNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ActivityLevelNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    internal class LossAmountNotFoundException : Exception
+    {
+        public LossAmountNotFoundException()
+        {
+        }
+
+        public LossAmountNotFoundException(string message) : base(message)
+        {
+        }
+
+        public LossAmountNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected LossAmountNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    internal class GenderNotFoundException : Exception
+    {
+        public GenderNotFoundException()
+        {
+        }
+
+        public GenderNotFoundException(string message) : base(message)
+        {
+        }
+
+        public GenderNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected GenderNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
